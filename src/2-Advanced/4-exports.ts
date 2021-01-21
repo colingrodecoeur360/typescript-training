c9277d7865a43ddf5490f2279cad925e45d81af0
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2021-01-21 12:00:46 +0100

    [Advanced] Exports

---
+++ src/2-Advanced/4-exports.ts
@@ -0,0 +1,10 @@
+/** PART 1: named export */
+export const a = "a";
+
+/** PART 2: default export */
+const b = "b";
+export default b;
+
+/** PART 3 - CommonJs default export */
+const c = "c";
+// export = c;
