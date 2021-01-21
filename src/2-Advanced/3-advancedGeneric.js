a41850a861ae9a64889665788ce9789ce29aa65d
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2021-01-21 11:13:14 +0100

    [Advanced] Notifier

---
+++ src/2-Advanced/3-advancedGeneric.js
@@ -0,0 +1,29 @@
+// question 1: type this
+// question 2: how would you add an indefinite number of arguments instead of just 1
+
+function buildNotifier() {
+    const observers = [];
+
+    return {
+        notify(arg) {
+            observers.map((observer) => observer(arg));
+        },
+        subscribe(observer) {
+            observers.push(observer);
+        },
+    };
+}
+
+const userCreatedNotifier = buildNotifier();
+userCreatedNotifier.subscribe(onUserCreated);
+createUser();
+createUser();
+
+function createUser() {
+    //...
+    const userId = Math.round(Math.random() * 100);
+    userCreatedNotifier.notify(userId);
+}
+function onUserCreated(id) {
+    console.log(`User ${id} created`);
+}
